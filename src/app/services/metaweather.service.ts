440c1dc6505f679cd1473579345280fbbde3490f
Daniel Trattner <dat@intern.frey-wille.com>
2018-10-09 17:15:24 +0200

    changed using own cors-anywhere service

--- src/app/services/metaweather.service.ts
+++ src/app/services/metaweather.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetaweatherService {
 
+  private cors_anywhere: string = 'https://cors-anywhere.mellon.cloud/';
   private api: string = 'https://www.metaweather.com/api';
+
+  private headers = {
+    "Origin": "https://umbrella.mellon.cloud",
+    "X-Requested-With": "XMLHttpRequest"
+  }
 
   constructor(private http: HttpClient) { }
 
   location(longitude: any, latitude: any) {
-    return this.http.get(`${this.api}/location/search/?lattlong=${longitude},${latitude}`);
+    return this.http.get(`${this.cors_anywhere}${this.api}/location/search/?lattlong=${longitude},${latitude}`,{headers:this.headers});
   }
 
   weather(woeid: number) {
-    return this.http.get(`${this.api}/location/${woeid}`);
+    return this.http.get(`${this.api}/location/${woeid}`,{headers:this.headers});
   }
 
 }
